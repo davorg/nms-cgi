0988a96c80f7d516583cc9747647b70792341d2e
gellyfish <gellyfish@9c5a2652-f0e4-4643-a6e0-4ada6f12299e>
2001-11-13 16:36:32 +0000

    Added a doc directory
    git-svn-id: https://nms-cgi.svn.sourceforge.net/svnroot/nms-cgi@30 9c5a2652-f0e4-4643-a6e0-4ada6f12299e

---
+++ branches/doc/doc/nms-cgi/ssi_rand_image/ssi_rand_image.pl
@@ -0,0 +1,74 @@
+#!/usr/bin/perl -Tw
+#
+# $Id: ssi_rand_image.pl,v 1.1.1.1 2001-11-13 16:36:30 gellyfish Exp $
+#
+# $Log: not supported by cvs2svn $
+# Revision 1.3  2001/11/13 09:18:20  gellyfish
+# Added CGI::Carp
+#
+# Revision 1.2  2001/11/11 17:55:27  davorg
+# Small amount of post-import tidying :)
+#
+# Revision 1.1.1.1  2001/11/11 16:48:57  davorg
+# Initial import
+#
+
+use strict;
+use POSIX 'strftime';
+use CGI qw(header img a);
+use CGI::Carp qw(fatalsToBrowser);
+use Fcntl qw(:DEFAULT :flock);
+
+# Configuration
+
+my $basedir = 'http://your.host.xxx/path/to/images/';
+
+my @images = ({ file => 'first_image.gif',
+		url => 'http://url_linked/to/first_image',
+		alt => 'First WWW Page' },
+	      { file => 'second_image.jpg',
+		url => 'http://url_linked/to/second_image',
+		alt => 'Second WWW Page' },
+	      { file => 'third_image.gif',
+		url => 'http://url_linked/to/third_image',
+		alt => 'Third WWW Page' });
+
+my $uselog = 1;
+my $logfile = '/path/to/log/file';
+
+my $date_fmt = '%c';
+my $date = strftime $date_fmt, localtime;
+
+my $link_image = 1;
+my $align = 'left';
+my $border = 2;
+
+# End configuration
+
+my $img = $images[rand(@images)];
+
+# Print Out Header With Random Filename and Base Directory
+print header;
+
+my $output = img({-src => $img->{file},
+		  -alt => $img->{alt},
+		  -border => $border,
+		  -align => $align});
+
+if ($link_image && $img->{url}) {
+  $output = a({-href => $img->{url}},
+	      $output);
+}
+
+print $output;
+
+# If You want a log, we add to it here.
+if ($uselog) {
+  sysopen(LOG, $logfile, O_APPEND|O_CREAT|O_RDWR)
+    or die "Can't open log file: $!\n";
+  flock LOG, LOCK_EX
+    or die "Can't lock log file: $!\n";
+
+  print LOG "$img->{file} - $date - $ENV{REMOTE_HOST}\n";
+  close(LOG);
+}
