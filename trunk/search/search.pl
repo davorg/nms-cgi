7fc77b4ee98400881a5bd50034351e73b58ff184
nickjc <nickjc@9c5a2652-f0e4-4643-a6e0-4ada6f12299e>
2002-04-09 20:26:32 +0000

    * Replaced out of date hard coded version number with a   CVS Revision tag
    git-svn-id: https://nms-cgi.svn.sourceforge.net/svnroot/nms-cgi@302 9c5a2652-f0e4-4643-a6e0-4ada6f12299e

--- trunk/search/search.pl
+++ trunk/search/search.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl -Tw
 #
-# $Id: search.pl,v 1.27 2002-03-27 20:36:40 davorg Exp $
+# $Id: search.pl,v 1.28 2002-04-09 20:26:32 nickjc Exp $
 #
 
 use strict;
@@ -17,7 +17,7 @@ $CGI::POST_MAX = $CGI::POST_MAX = 4096;
 
 # PROGRAM INFORMATION
 # -------------------
-# search.pl v1.17
+# search.pl $Revision $
 #
 # This program is licensed in the same way as Perl
 # itself. You are free to choose between the GNU Public
