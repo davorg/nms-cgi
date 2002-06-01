1a472510c411891a97ec560d9deada87d5256888
davorg <davorg@9c5a2652-f0e4-4643-a6e0-4ada6f12299e>
2002-06-01 11:56:15 +0000

    Initial Version
    git-svn-id: https://nms-cgi.svn.sourceforge.net/svnroot/nms-cgi@390 9c5a2652-f0e4-4643-a6e0-4ada6f12299e

---
+++ trunk/perldiver/perldiver.pl
@@ -0,0 +1,121 @@
+#!/usr/bin/perl -w
+
+use strict;
+use File::Find;
+
+my $plocation  = join('<br />', split(' ', `whereis perl`));
+my $sendmail   = join('<br />', split(' ', `whereis sendmail`));
+my $inc        = join('<br />', @INC);
+
+my $dev = 'nms';
+my $program = 'perldiver';
+my $version = "1.1";
+
+my $env;
+foreach (keys %ENV){
+  $env .= qq(<tr><th width="35%" valign="top">$_</th><td width="65%" valign="top">$ENV{$_}</td></tr>\n);
+}
+
+my @foundmods;
+find(\&wanted, @INC);
+
+my %found;
+++$found{$_} foreach @foundmods;
+
+@foundmods = sort keys(%found);
+my $modcount = @foundmods;
+
+my $third = int(@foundmods / 3);
+
+push @foundmods, '' while @foundmods % 3;
+
+my $mods;
+for (0 .. $third) {
+  $mods .= qq(<tr><td width="33%">$foundmods[$_]</td><td width="33%">$foundmods[$_ + $third + 1]</td><td width="33%">$foundmods[$_ + 2 + (2 * $third)]</td></tr>\n);
+}
+
+print "Content-type:  text/html\n\n";
+
+my $page = do { local $/; <DATA>};
+
+$page =~ s/\[%\s*(\S+)\s*%]/$1/eeg;
+
+print $page;
+
+exit;
+
+sub wanted {
+  if ($File::Find::name =~ /\.pm$/){
+    open(MODFILE, $File::Find::name) || return;
+    while(<MODFILE>){
+      if (/^ *package +(\S+);/) {
+	push (@foundmods, $1);
+	last;
+      }
+    }
+  }
+}
+__END__
+<?xml version="1.0" encoding="iso-8859-1"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>[% $program %] [% $version %]</title>
+<style type="text/css">
+body { font-family: Verdana, sans serif;
+       font-weight: normal; }
+th { background-color: ghostwhite;
+     text-align: left;
+     font-weight: bold; }
+</style>
+</head>
+<body>
+
+<h1>[% $program %] [% $version %]</h1>
+<h2>Server Program Paths</h2>
+<table border="0" cellpadding="3" width="95%">
+<tr>
+<th valign="top" width="35%">Perl Executable:</th>
+<td valign="top">[% $^X %]</td>
+</tr>
+<tr>
+<th valign="top">Perl Version:</th>
+<td valign="top">[% $] %]</td>
+</tr>
+<tr>
+<th valign="top">Perl compile version OS:</th>
+<td valign="top">[% $^O %]</td>
+</tr>
+<tr>
+<th valign="top">GID: <span style="font-size: smaller; font-weight:normal">(If not blank, you are on a machine that supports membership in multiple groups simultaneously)</span></th>
+<td valign="top">[% $< %]</td>
+</tr>
+<tr>
+<th valign="top">Location of Perl:</th>
+<td valign="top">[% $plocation %]</td>
+</tr>
+<tr>
+<th valign="top">Location of Sendmail:</th>
+<td valign="top">[% $sendmail %]</td>
+</tr>
+<tr>
+<th valign="top">Directory locations searched for perl modules</th>
+<td valign="top">[% $inc %]</td>
+</tr>
+</table>
+
+<h2>Environment Variables</h2>
+<table border="0" cellpadding="3" width="95%">[% $env %]</table>
+
+<h2>Installed Modules</h2>
+<p>[% $modcount %] modules found</p>
+<table border="0" cellpadding="3" width="95%">
+[% $mods %]
+</table>
+<hr />
+<address>perldiver is Copyright &copy; 2002, London Perl Mongers. Latest
+version of this and many other scripts is always available from
+<a href="http://nms-cgi.sourceforge.net/">the <i>nms</i> project</a>.</address>
+</body>
+</html>
