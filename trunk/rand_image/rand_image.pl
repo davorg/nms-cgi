fe159561e9366094606cbfe4047188cbb82a2954
gellyfish <gellyfish@9c5a2652-f0e4-4643-a6e0-4ada6f12299e>
2002-07-04 08:22:48 +0000

    Made an option to emit the image directly rather than redirect
    git-svn-id: https://nms-cgi.svn.sourceforge.net/svnroot/nms-cgi@422 9c5a2652-f0e4-4643-a6e0-4ada6f12299e

--- trunk/rand_image/rand_image.pl
+++ trunk/rand_image/rand_image.pl
@@ -1,10 +1,10 @@
 #! /usr/bin/perl -wT
 #
-# $Id: rand_image.pl,v 1.7 2002-03-27 20:36:37 davorg Exp $
+# $Id: rand_image.pl,v 1.8 2002-07-04 08:22:48 gellyfish Exp $
 #
 
 use strict;
-use CGI qw(redirect);
+use CGI qw(redirect header);
 use Fcntl qw(:DEFAULT :flock);
 use vars qw($DEBUGGING $done_headers);
 
@@ -21,18 +21,39 @@ BEGIN
    $DEBUGGING = 1;
 }
    
-my $basedir = 'http://your.host.com/images/';
+# If this is set to 1 then the program will issue a redirect to the image
+# and $baseurl must be the beginning of the URI where the images reside.
+# This might not work on all browsers.
+# If it is set to 0 then the program will send the image to the browser
+# directly (which is more costly for the server) in which case $basedir
+# must be the full system path to the directory where the image files are.
+# It might be necessary to add new extenstion => content type pairs to
+# the %content_type below if you are using file extenstions that are not
+# among the defaults.
+`
+my $use_redirect = 0;
+my $baseurl = 'http://nms-test.gellyfish.com/images/';
+my $basedir = '/var/www/nms-test/images/';
 
-my @files = qw(first_image.gif
-	       test.gif
-	       random.gif
-	       neat.jpg);
+# Your image files here.
 
-my $uselog = 1; # 1 = YES; 0 = NO
+my @files = qw(
+               foo.jpg
+               bah.png
+              );
+
+my $uselog = 0; # 1 = YES; 0 = NO
 my $logfile = '/path/to/piclog';
 
 # End configuration
 
+# Might need to add to content types mapping extensions 
+
+my %content_types = (
+                       jpg => 'image/jpeg',
+                       gif => 'image/gif',
+                       png => 'image/png'
+                    );
 
 # We need finer control over what gets to the browser and the CGI::Carp
 # set_message() is not available everywhere :(
@@ -86,10 +107,6 @@ EOERR
    $SIG{__DIE__} = \&fatalsToBrowser;
 }   
 
-if ( $basedir !~ m%/$% )
-{
-   $basedir .= '/';
-}
 
 my $pic = $files[rand @files];
 
@@ -104,5 +121,33 @@ if ($uselog) {
     or die "Can't close logfile: $!\n";
 }
 
-print redirect("$basedir$pic");
-$done_headers++;
+if ( $use_redirect )
+{
+   if ( $baseurl !~ m%/$% )
+   {
+      $baseurl .= '/';
+   }
+    print redirect("$baseurl$pic");
+    $done_headers++;
+}
+else
+{
+   if ( $basedir !~ m%/$% )
+   {
+      $basedir .= '/';
+   }
+
+   my ( $extension ) = $pic =~ /\.(\S+)$/;
+
+   my $ctype = $content_types{$extension} || 'image/png';
+
+   open INFILE, "<$basedir$pic" or die "Can't open $basedir$pic - $!";
+   binmode INFILE;
+   local $/;
+   
+   my $image = <INFILE>;
+   close INFILE;
+ 
+    print header(-type => $ctype),
+          $image;
+}
