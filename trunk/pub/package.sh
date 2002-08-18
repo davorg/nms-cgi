91cf90a63d569573a46e318b374281fc6c67cb00
davorg <davorg@9c5a2652-f0e4-4643-a6e0-4ada6f12299e>
2002-08-18 08:53:00 +0000

    Added code to produce .VER files
    git-svn-id: https://nms-cgi.svn.sourceforge.net/svnroot/nms-cgi@467 9c5a2652-f0e4-4643-a6e0-4ada6f12299e

--- trunk/pub/package.sh
+++ trunk/pub/package.sh
@@ -13,3 +13,6 @@ unix2dos -k `cat $p/MANIFEST`
 zip $p.zip -@ < $p/MANIFEST 
 dos2unix -k $p/MANIFEST
 dos2unix -k `cat $p/MANIFEST`
+
+main=`head -1 $p/MANIFEST`;
+grep '$Id: package.sh,v 1.2 2002-08-18 08:53:00 davorg Exp $main | head -1 | perl -ne '/\$Id: package.sh,v 1.2 2002-08-18 08:53:00 davorg Exp $1' > $p.VER
